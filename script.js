4062ddf482e112750078a458a13051426507ee12
shubhamSG24 <gaikwadshubhams@gmail.com>
2023-01-31 21:45:01 +0530

    landscape

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ let weather = {
         document.querySelector(".humidity").innerText = "Humidity :" + humidity + "%";
         document.querySelector(".wind").innerText = "WindSpeed :" + speed + "Km/hr";
         document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage ="url('https://source.unsplash.com/1920x1080/?" + name + "')";
+        document.body.style.backgroundImage ="url('https://source.unsplash.com/1920x1080/?" + name + ",landscape')";
 
     },
     search: function () {
