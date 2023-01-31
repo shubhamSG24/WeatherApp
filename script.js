4dae7f00f9b9b70e0d3c5c93364ea43bd8c3a117
shubhamSG24 <gaikwadshubhams@gmail.com>
2023-01-31 21:41:34 +0530

    picture

--- script.js
+++ script.js
@@ -15,15 +15,17 @@ let weather = {
         const { icon, description} = data.weather[0];
         const { temp, humidity} = data.main;
         const { speed } = data.wind;
-        console.log(name,icon,description,temp,humidity,speed);
-        document.querySelector(".Weather").innerText = " Weather in ";
-        document.querySelector(".city").innerText = name;
+        document.querySelector(".Weather").innerText = " Weather in "+name;
+      
         document.querySelector(".icon").src = 
         "https://openweathermap.org/img/wn/"+ icon +"@2x.png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity :" + humidity + "%";
         document.querySelector(".wind").innerText = "WindSpeed :" + speed + "Km/hr";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage ="url('https://source.unsplash.com/1920x1080/?" + name + "')";
+
     },
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
@@ -33,4 +35,11 @@ let weather = {
 document.querySelector(".search button").addEventListener("click", function(){
         weather.search();
     });
-    
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+        if (event.key == "Enter") {
+            weather.search();
+        } 
+    })   
+
+weather.fetchWeather("Pune");
