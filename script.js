4aa3a9dea107a9e9f6224f4438ec5255af9eafce
shubhamSG24 <gaikwadshubhams@gmail.com>
2023-02-02 20:45:11 +0530

    final

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ let weather = {
         const { temp, humidity} = data.main;
         const { speed } = data.wind;
         document.querySelector(".Weather").innerText = " Weather in "+name;
-      
         document.querySelector(".icon").src = 
         "https://openweathermap.org/img/wn/"+ icon +"@2x.png";
         document.querySelector(".description").innerText = description;
